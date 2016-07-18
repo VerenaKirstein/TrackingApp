c0ba0886161ece023382fd567cbaac549b813d00
thorsten.kelm@hs-bochum.de <thorsten.kelm@hs-bochum.de>
2016-07-18 13:16:50 +0200

    Reformated Code, Add comments

--- app/src/main/java/de/hsbo/veki/trackingapp/QueryFeatureLayer.java
+++ app/src/main/java/de/hsbo/veki/trackingapp/QueryFeatureLayer.java
@@ -58,7 +58,7 @@ public class QueryFeatureLayer extends AsyncTask<String, Void, FeatureResult> {
             MainActivity.graphicsLayer.removeAll();
 
             // Define a new marker symbol for the result graphics
-            SimpleMarkerSymbol sms = new SimpleMarkerSymbol(Color.RED, 10, SimpleMarkerSymbol.STYLE.CIRCLE);
+            SimpleMarkerSymbol sms = new SimpleMarkerSymbol(Color.BLUE, 10, SimpleMarkerSymbol.STYLE.CIRCLE);
 
             // iterate through results
             for (Object element : results) {
